cd5ae1a744c14aa3216e55b93d37d16350de5f7d
Mykyta <sayron.arnorius@yahoo.com>
2023-10-23 00:21:10 +0200

    Initial Commit:

---
+++ src/main/java/com/nikita/springbootpj/SpringBootPjApplication.java
@@ -0,0 +1,13 @@
+package com.nikita.springbootpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPjApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/nikita/springbootpj/entities/enums/UserType.java
@@ -0,0 +1,6 @@
+package com.nikita.springbootpj.entities.enums;
+
+public enum UserType {
+    ADMIN,
+    CUSTOMER
+}
